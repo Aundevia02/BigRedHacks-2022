87a562a083c00e87d4cf1d5551b1ce3a1f8f5127
Will Huey <wph52@cornell.edu>
2022-10-15 16:49:31 -0400

    enny >/3

--- carbon-analysis/parse.py
+++ carbon-analysis/parse.py
@@ -13,6 +13,8 @@ def parseIngredient(ingredient_line, ingredient):
     grams = {"teaspoon": 5, "tablespoon": 14.8, "fluid ounce": 30,
              "cup": 200, "pint": 450, "quart": 900, "gallon": 3500}
 
+    discretes = {"eggs": 40, "egg": 40, "bananas": 110, "banana": 110}
+
     for measure in measures:
         if measure in ingredient_line or (measure + "s") in ingredient_line:
             amount = ingredient_line[:ingredient_line.find(measure)]
@@ -21,46 +23,51 @@ def parseIngredient(ingredient_line, ingredient):
             print("measure: " + measure)
             if "/" in amount:
                 value = float(sum(Fraction(s) for s in amount.split()))
-                return value
+                return value * grams[measure]
             if "and" in amount:
                 value = float(sum(Fraction(s) for s in amount.split(" and ")))
-                return value
+                return value * grams[measure]
             for c in amount:
                 if ord(c) == 188:
-                    return 1/4
+                    return 1/4 * grams[measure]
                 elif ord(c) == 189:
-                    return 1/2
+                    return 1/2 * grams[measure]
                 elif ord(c) == 190:
-                    return 3/4
-            return amount
+                    return 3/4 * grams[measure]
+            return int(amount) * grams[measure]
 
     if not any(char.isdigit() for char in ingredient_line):
         return 0
     # no measure included
     amount = ingredient_line[:ingredient_line.find(ingredient)]
     values = amount.split()
+    if ingredient in discretes:
+        return discretes[ingredient] * int(values[0])
     return int(values[0])
 
 
-lines = [
-    "2 teaspoons baking soda",
-    "2 cups milk",
-    "3 1/2 tablespoons sugar",
-    "½ teaspoon salt",
-    "4 bananas",
-    "4 tablespoons oil",
-    "2 cups flour",
-    "2 eggs"
-]
+def printLines():
+    lines = [
+        "2 teaspoons baking soda",
+        "2 cups milk",
+        "3 1/2 tablespoons sugar",
+        "½ teaspoon salt",
+        "4 bananas",
+        "4 tablespoons oil",
+        "2 cups flour",
+        "2 eggs"
+    ]
 
-ingredients = [
-    "Baking soda",
-    "Milk", "Sugar", "Salt", "Bananas", "Oil", "flour", "Eggs"
-]
+    ingredients = [
+        "Baking soda",
+        "Milk", "Sugar", "Salt", "Bananas", "Oil", "flour", "Eggs"
+    ]
+    for i in range(len(lines)):
+        print(
+            f"Ingredient: {ingredients[i]} \nIngredient line: {lines[i]} \ngrams: {parseIngredient(lines[i],ingredients[i])}")
 
-for i in range(len(lines)):
-    print(
-        f"Ingredient: {ingredients[i]} \nIngredient line: {lines[i]} \ngrams: {parseIngredient(lines[i],ingredients[i])}")
+
+printLines()
 
 
 def parseIngredientGPT(ingredient_line, ingredient):
--- carbon-analysis/score.py
+++ carbon-analysis/score.py
@@ -4,61 +4,16 @@ from fuzzywuzzy import fuzz
 from fuzzywuzzy import process
 
 # https://www.geeksforgeeks.org/how-to-do-fuzzy-matching-on-pandas-dataframe-column-using-python/
-
-
-def matchNames(to_match, data):
-    data_list = data['Item'].tolist()
-
-    mat1 = []
-    mat2 = []
-    p = []
-
-    # printing the pandas dataframes
-    print("First dataframe:\n", dframe1,
-          "\nSecond dataframe:\n", dframe2)
-
-    # converting dataframe column to list
-    # of elements
-    # to do fuzzy matching
-    list1 = dframe1['name'].tolist()
-    list2 = dframe2['name'].tolist()
-
-    # taking the threshold as 82
-    threshold = 82
-
-    # iterating through list1 to extract
-    # it's closest match from list2
-    for i in list1:
-        mat1.append(process.extract(i, list2, limit=2))
-    dframe1['matches'] = mat1
-
-    # iterating through the closest matches
-    # to filter out the maximum closest match
-    for j in dframe1['matches']:
-        for k in j:
-            if k[1] >= threshold:
-                p.append(k[0])
-        mat2.append(",".join(p))
-        p = []
-
-    # storing the resultant matches back to dframe1
-    dframe1['matches'] = mat2
-    print("\nDataFrame after Fuzzy matching:")
-    dframe1
-
-    threshold = 82
-    print(data_list)
-
-
 carbon_data = pd.read_csv("carbon-data.csv")
 print(carbon_data.head())
+print(carbon_data.loc[carbon_data['Item'] == 'BEER IN CAN'])
 
 
-def getScore(ingredientLines, ingredients):
+def getScore(ingredientLines, ingredients, servings):
     # creating a data frame
     carbon_data = pd.read_csv("carbon-data.csv")
-    print(df.head())
+    print(carbon_data.head())
 
     parsed_ingredients = []
-    # for idx, line in enumerate(ingredientLines):
-    #     parsed_ingredients.append(parseIngredient(line, ingredients[idx]))
+    for idx, line in enumerate(ingredientLines):
+        parsed_ingredients.append(parseIngredient(line, ingredients[idx]))
