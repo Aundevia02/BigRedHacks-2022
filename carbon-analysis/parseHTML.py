1fcc8531c23ce91094e62c663aeb17a01f52e4ba
Benny Rubin <bcr57@cornell.edu>
2022-10-15 23:48:36 -0400

    mostly working?

--- carbon-analysis/parseHTML.py
+++ carbon-analysis/parseHTML.py
@@ -25,10 +25,7 @@ def get_servings(html):
 
 def get_ingredients(html):
     match = '<span data-ingredient-quantity="true">(.*)<\/span>.*<span data-ingredient-unit="true">(.*)<\/span>.*<span data-ingredient-name="true">(.*)<\/span>'
-
+    # print(type(html))
     matches = re.findall(match, html)
 
     return matches
-
-
-test_get_ingredients()
