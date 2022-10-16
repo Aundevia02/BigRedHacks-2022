55271bb3e8eec207df2eb882e7963c37fc26d9b9
Akshay Undevia <asu7@cornell.edu>
2022-10-15 22:22:32 -0400

    Implement backend query

--- Elea/src/popup.js
+++ Elea/src/popup.js
@@ -1,6 +1,11 @@
 chrome.runtime.onMessage.addListener(function (request, sender) {
   if (request.action == "getSource") {
-    message.innerText = request.source;
+    // message.innerText = request.source;
+    message.innerText = getTitle(request.source);
+
+    var rawString = fetch('http://10.49.66.241:5001/query');
+    // var json = JSON.parse(rawString);
+    alert(rawString);
   }
 });
 
@@ -20,3 +25,12 @@ function onWindowLoad() {
 }
 
 window.onload = onWindowLoad;
+
+function getTitle(html_string) {
+  var first_string = '<h1 id="article-heading_1-0" class="comp type--lion article-heading mntl-text-block">';
+  var part = html_string.substring(
+    html_string.indexOf(first_string) + first_string.length + 1,
+    html_string.indexOf("</h1>")
+  );
+  return part;
+}
